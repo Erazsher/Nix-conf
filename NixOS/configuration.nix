bade72468308fcff9b4bab7fd0d412c49114eb21
Erazsher <83161571+Erazsher@users.noreply.github.com>
2021-11-21 09:22:30 +0000

    Nix conf with light dm & openbox

---
+++ NixOS/configuration.nix
@@ -0,0 +1,280 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+
+   networking.hostName = "Nix-top"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    networking.networkmanager.enable = true; 
+  # Set your time zone.
+   time.timeZone = "Asia/Kolkata";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp1s0.useDHCP = true;
+  networking.interfaces.wlp2s0.useDHCP = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+   i18n.defaultLocale = "en_US.UTF-8";
+   console = {
+     font = "Lat2-Terminus16";
+     keyMap = "us";
+   };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+
+  # Enable the XFCE Desktop Environment.
+  services.xserver.displayManager.lightdm.enable = true;
+  #services.xserver.desktopManager.xfce.enable = true;
+  hardware.opengl.driSupport32Bit = true;
+   services.xserver.videoDrivers = [ "intel" ];
+   services.xserver.deviceSection = ''
+     Option "DRI" "2"
+     Option "TearFree" "true"
+   '';
+  # recomended by Nix-manual
+  # services.xserver.videoDrivers = [ "modesetting" ];
+  # services.xserver.useGlamor = true;
+  
+  # Configure keymap in X11
+   services.xserver.layout = "us";
+   services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+  # Bluetooth
+    hardware.bluetooth.enable = true;
+
+  # Enable sound.
+  # sound.enable = false;
+  # hardware.pulseaudio.enable = true;
+  
+  # pipewire
+  # rtkit is optional but recommended
+    security.rtkit.enable = true;
+    services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  # If you want to use JACK applications, uncomment this
+  #jack.enable = true;
+
+  # use the example session manager (no others are packaged yet so this is enabled by default,
+  # no need to redefine it in your config for now)
+  #media-session.enable = true;
+  };
+  
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
+
+  services.pipewire  = {
+  media-session.config.bluez-monitor.rules = [
+    {
+      # Matches all cards
+      matches = [ { "device.name" = "~bluez_card.*"; } ];
+      actions = {
+        "update-props" = {
+          "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
+          # mSBC is not expected to work on all headset + adapter combinations.
+          "bluez5.msbc-support" = true;
+          # SBC-XQ is not expected to work on all headset + adapter combinations.
+          "bluez5.sbc-xq-support" = true;
+        };
+      };
+    }
+    {
+      matches = [
+        # Matches all sources
+        { "node.name" = "~bluez_input.*"; }
+        # Matches all outputs
+        { "node.name" = "~bluez_output.*"; }
+      ];
+      actions = {
+        "node.pause-on-idle" = false;
+      };
+    }
+  ];
+};
+  
+  services.pipewire = {
+  config.pipewire = {
+    "context.objects" = [
+      {
+        # A default dummy driver. This handles nodes marked with the "node.always-driver"
+        # properyty when no other driver is currently active. JACK clients need this.
+        factory = "spa-node-factory";
+        args = {
+          "factory.name"     = "support.node.driver";
+          "node.name"        = "Dummy-Driver";
+          "priority.driver"  = 8000;
+        };
+      }
+      {
+        factory = "adapter";
+        args = {
+          "factory.name"     = "support.null-audio-sink";
+          "node.name"        = "Microphone-Proxy";
+          "node.description" = "Microphone";
+          "media.class"      = "Audio/Source/Virtual";
+          "audio.position"   = "MONO";
+        };
+      }
+      {
+        factory = "adapter";
+        args = {
+          "factory.name"     = "support.null-audio-sink";
+          "node.name"        = "Main-Output-Proxy";
+          "node.description" = "Main Output";
+          "media.class"      = "Audio/Sink";
+          "audio.position"   = "FL,FR";
+        };
+      }
+    ];
+  };
+};
+  
+
+  # Enable touchpad support (enabled default in most desktopManager).
+   services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+   users.users.san = {
+     isNormalUser = true;
+     extraGroups = [ "wheel" "networkmanager"]; # Enable ‘sudo’ for the user.
+   };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+   environment.systemPackages = with pkgs; [
+     # cli-tools
+     vim 
+     wget
+     curl
+     git
+     neofetch
+     htop
+     # gui-tools
+     libreoffice
+     celluloid
+     firefox
+     keepassxc
+     #Extras
+     tlp
+     papirus-icon-theme
+     arc-theme 
+     blueman
+     pavucontrol 
+     rofi
+     archiver
+     wine-staging
+     intel-media-driver
+     microcodeIntel
+     xorg.xf86videointel
+     #Window-manager
+     openbox
+     termite
+     obconf
+     lxappearance
+     menumaker
+     nitrogen    
+     picom
+     tint2
+     gnome.nautilus
+   ];
+
+   nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+   };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+  services.blueman.enable = true;
+  services.xserver.windowManager.openbox.enable = true;
+  services.gvfs.enable = true;
+  # tlp
+  # services.tlp.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_BAT="powersave";
+      CPU_SCALING_GOVERNOR_ON_AC="performance";
+
+      # The following prevents the battery from charging fully to
+      # preserve lifetime. Run `tlp fullcharge` to temporarily force
+      # full charge.
+      # https://linrunner.de/tlp/faq/battery.html#how-to-choose-good-battery-charge-thresholds
+      START_CHARGE_THRESH_BAT0=40;
+      STOP_CHARGE_THRESH_BAT0=50;
+
+      # 100 being the maximum, limit the speed of my CPU to reduce
+      # heat and increase battery usage:
+      CPU_MAX_PERF_ON_AC=90;
+      CPU_MAX_PERF_ON_BAT=60;
+    };
+  };
+
+  # To get rid of old generations
+  nix.gc = {
+  automatic = true;
+  dates = "weekly";
+  options = "--delete-older-than 30d";
+  };
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.05"; # Did you read the comment?
+
+}
+
